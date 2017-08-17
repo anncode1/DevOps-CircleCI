bae1e7179cd951a8a505ef4ac0ce3dc25e64376a
Anahi Salgado <anahi@platzi.com>
2017-08-17 11:05:22 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	return res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/*', (req, res) => {
+	return res.redirect('/');
+});
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,5 @@
+const http = require('http');
+const app = require('./app');
+
+http.createServer(app).listen(3000);
+console.log('Im Runing');
